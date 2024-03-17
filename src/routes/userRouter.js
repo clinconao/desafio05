ada7777306ec6a3370c6af166a282fdf1e735544
clinconao <cl.linconao@gmail.com>
2024-03-17 13:15:13 +1300

    nueva actualicacion

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -14,8 +14,8 @@ userRouter.get('/', async (req, res) =>{
 
 userRouter.post('/', async (req,res) =>{
     try {   
-        const {nombre, apellido, email, edad, password} = req.body
-        const resultado = await userModel.create({nombre, apellido, email, edad, password})
+        const {first_name, last_name, email, password, age} = req.body
+        const resultado = await userModel.create({first_name, last_name, email, password, age})
         res.status(201).send(resultado)
     } catch (error) {
         res.status(500).send("Error al consultar usuarios: ", e)
